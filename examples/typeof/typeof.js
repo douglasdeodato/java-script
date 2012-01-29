c1562793dd23096ab71d963861af356135352ffe
Douglas Deodato <douglasdeodato@gmail.com>
2012-01-29 19:12:56 +0000

    write variables

--- examples/typeof/typeof.js
+++ examples/typeof/typeof.js
@@ -1,2 +1,32 @@
-var s = "hello hello hello"
-console.log(typeof s);
+var d = "hello hello hello"
+document.writeln(typeof d );
+document.write("<br>");
+
+var o = 10;
+document.writeln(typeof o);
+document.write("<br>");
+
+var a = {foo: "bar"};
+document.write(typeof a);
+document.write("<br>");
+
+var f = function(){};
+document.write(typeof f);
+document.write("<br>");
+
+document.write(typeof null);
+document.write("<br>");
+
+var re = /abc/;
+document.write(typeof re);
+document.write("<br>");
+
+var a = [1,2,3];
+document.write(typeof a);
+document.write("<br>");
+
+var dog = function(){};
+var dog = new dog();
+document.write(typeof dog);
+ 
+
